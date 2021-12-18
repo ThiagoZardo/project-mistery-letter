058685c22a2b37728690299f2839c8cb7422bc51
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2021-12-18 17:06:14 -0300

    Requisitos de 1 a 4 OK

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
 let caixaTexto = document.getElementById('carta-texto');
-const cartaGerada = document.getElementById('carta-gerada');
+let cartaGerada = document.getElementById('carta-gerada');
 let btnCriarcarta = document.getElementById('criar-carta');
+let spanDinamico = cartaGerada.childNodes;
 
 btnCriarcarta.addEventListener('click',criarCarta);
 
+
+
 function criarCarta(){
-    cartaGerada.innerText = caixaTexto.value;
+    //Remove todos os span 
+    for(let p = spanDinamico.length-1; p >= 0; p-=1){
+        spanDinamico[p].remove();
+    }
+    
+    //Cria novos span colocando uma letra em cada
+    for(let i = 0; i < caixaTexto.value.split(" ").length; i+=1){
+        let span = document.createElement("span");
+        cartaGerada.appendChild(span)[i];
+        span.innerHTML = caixaTexto.value.split(" ")[i];
+    }    
 }
+
